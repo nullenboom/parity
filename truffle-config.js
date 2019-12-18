971b1208af1071ef2b053a18bd7a7e4bdff72a08
Niklas Ullenboom <niklas.ullenboom@paluno.uni-due.de>
2019-12-18 15:02:44 +0100

    changed truffle config to 8545 port

--- truffle-config.js
+++ truffle-config.js
@@ -2,11 +2,11 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 7545,
-      network_id: "*", // Match any network id
+      port: 8545,
+      network_id: "*" // Match any network id
     }
   },
-  compilers: {
+   compilers: {
     solc: {
       settings: {
         optimizer: {
