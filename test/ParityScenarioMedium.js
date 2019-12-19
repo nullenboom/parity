ec71b4adf591fae6dcf7733e8c89448e3308a045
Niklas Ullenboom <niklas.ullenboom@paluno.uni-due.de>
2019-12-19 10:10:28 +0100

    use medium test for example project instead of small

--- test/ParityScenarioMedium.js
+++ test/ParityScenarioMedium.js
@@ -14,3 +14,18 @@ contract("Parity_Modulo", async accounts => {
     const parity = await modulo.checkParity(10, 20);
   });
 });
+
+contract("Parity_Binary", async accounts => {
+  it("Non-Parity Numbers Scenario", async () => {
+    const binary = await Binary.new();
+    const parity = await binary.checkParity(10, 5);
+  });
+});
+
+
+contract("Parity_Modulo", async accounts => {
+  it("Non-Parity Numbers Scenario", async () => {
+    const modulo = await Modulo.new();
+    const parity = await modulo.checkParity(10, 5);
+  });
+});
